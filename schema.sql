0210ed1d32d5b963bfc138a5a6076d45a42bc7a5
ramonejimenez <57235013+ramonejimenez@users.noreply.github.com>
2020-01-09 15:34:18 -0800

    01/09/20

--- schema.sql
+++ schema.sql
@@ -1,9 +1,18 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
+);
+
+CREATE TABLE Dept_Emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE employees (
@@ -21,9 +30,16 @@ dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE Titles (
+emp_no INT NOT NULL,
+title VARCHAR NOT NULL,
+from_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE salaries (
@@ -35,4 +51,4 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM dept_emp;
